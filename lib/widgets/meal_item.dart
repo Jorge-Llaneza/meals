dce6dbd9b5cda0d7f6ee311cbacc4bee9421597c
jorge <llanezarodriguezjorge@gmail.com>
2026-08-27 12:38:00 +0200

    add descriptions to meal items

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
   const MealItem({super.key, required this.meal});
 
   final Meal meal;
+
+  String get complexityText {
+    return _capitalizeFirstLetter(meal.complexity.name);
+  }
+
+  String get affordabilityText {
+    return _capitalizeFirstLetter(meal.affordability.name);
+  }
+
+  String _capitalizeFirstLetter(String string) {
+    return string[0].toUpperCase() + string.substring(1);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,11 +62,23 @@ class MealItem extends StatelessWidget {
                         color: Colors.white,
                       ),
                     ),
-                    const SizedBox(height: 12),
-                    Row(children: [
-
-                    ],
-                  ),
+                    const SizedBox(height: 8),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MealItemTrait(
+                          icon: Icons.schedule,
+                          label: '${meal.duration} min',
+                        ),
+                        const SizedBox(width: 12),
+                        MealItemTrait(icon: Icons.work, label: complexityText),
+                        const SizedBox(width: 12),
+                        MealItemTrait(
+                          icon: Icons.attach_money,
+                          label: affordabilityText,
+                        ),
+                      ],
+                    ),
                   ],
                 ),
               ),
