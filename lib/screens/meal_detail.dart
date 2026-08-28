100ac8814cf3ad889fef743b31c0bdce93885a85
jorge <llanezarodriguezjorge@gmail.com>
2026-08-28 15:54:52 +0200

    initial meal detail screen

--- lib/screens/meal_detail.dart
+++ lib/screens/meal_detail.dart
@@ -11,7 +11,23 @@ class MealDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(title)),
-      body: Text(meal.title, style: const TextStyle(color: Colors.white)),
+      body: Column(
+        children: [
+          Image.network(
+            meal.imageUrl,
+            width: double.infinity,
+            height: 300,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(height: 14),
+          Text(
+            'Ingredients',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
