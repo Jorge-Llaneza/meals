ba22920ea2506cae955e77a9f417493629df06ca
jorge <llanezarodriguezjorge@gmail.com>
2026-09-13 20:50:03 +0200

    move filtering logic to provider

--- lib/providers/filters_provider.dart
+++ lib/providers/filters_provider.dart
@@ -1,4 +1,6 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod/legacy.dart';
+import 'package:meals/providers/meals_provider.dart';
 import 'package:meals/screens/filters_screen.dart';
 
 class FiltersNotifier extends StateNotifier<Map<Filter, bool>> {
@@ -23,3 +25,24 @@ final filtersProvider =
     StateNotifierProvider<FiltersNotifier, Map<Filter, bool>>(
       (ref) => FiltersNotifier(),
     );
+
+final filteredMealsProvider = Provider((ref) {
+  final meals = ref.watch(mealProvider);
+  final activeFilters = ref.watch(filtersProvider);
+
+  return meals.where((meal) {
+    if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
+      return false;
+    }
+    if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
+      return false;
+    }
+    if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
+      return false;
+    }
+    if (activeFilters[Filter.vegan]! && !meal.isVegan) {
+      return false;
+    }
+    return true;
+  }).toList();
+});
