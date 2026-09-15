8db80909a4ac30a2275986be7a117ea2b238f99d
jorge <llanezarodriguezjorge@gmail.com>
2026-09-15 11:12:58 +0200

    finish implicit animation

--- lib/screens/meal_detail.dart
+++ lib/screens/meal_detail.dart
@@ -36,9 +36,15 @@ class MealDetailScreen extends ConsumerWidget {
             icon: AnimatedSwitcher(
               duration: Duration(milliseconds: 300),
               transitionBuilder: (child, animation) {
-                return RotationTransition(turns: animation, child: child);
+                return RotationTransition(
+                  turns: Tween(begin: 0.7, end: 1.0).animate(animation),
+                  child: child,
+                );
               },
-              child: Icon(isFavorite ? Icons.star : Icons.star_border),
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavorite),
+              ),
             ),
           ),
         ],
